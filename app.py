60c22668b8d6668cd2d119d11bf00bfe6f494e98
Nakul Srikanth <nakul_0702@yahoo.com>
2024-02-04 20:51:47 -0800

    Version 1.11.0

--- app.py
+++ app.py
@@ -26,10 +26,11 @@ def prompt():
     story_input = str.lower(story_input)
     st.divider()
     clicked = st.button("Learn!", type="primary", disabled=(True if (story_input == "" or language_selection == "") else False))
+    st.write("\n\n\n Protoype in-progress. © Property of Nakul Srikanth")
     if clicked:
         generate_text(language_selection, story_input, st.secrets["user_api_key"])
         st.write("*GPT may not always produce accurate responses.")
-        st.write("\n\n\n Protoype in-progress. © Property of Nakul Srikanth")
+    
             
 
 def display_text(txt):
